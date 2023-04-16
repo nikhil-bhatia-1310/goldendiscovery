8944a4a648cee836a6e67b28804e655a62ce7917
Nikhil Bhatia <nik.bhatia.13@gmail.com>
2023-04-16 13:30:17 +0530

    added cross origin

--- src/main/java/com/nik/golden/discovery/GoldendiscoveryApplication.java
+++ src/main/java/com/nik/golden/discovery/GoldendiscoveryApplication.java
@@ -3,9 +3,11 @@ package com.nik.golden.discovery;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @EnableEurekaServer
+@CrossOrigin("http://localhost:3000/")
 public class GoldendiscoveryApplication {
 
 	public static void main(String[] args) {
